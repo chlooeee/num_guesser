71b84084f6f2f3c9e92b494ac37393b9b01fcc0d
Chloe <csing@ucdavis.edu>
2019-09-24 18:54:48 -0700

    sinatra intro donezo :)

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+def check_guess(number, guess)
+    if guess - number < -5
+        "Way too low!"
+    elsif guess - number > 5
+        "way too high!"
+    elsif guess > number 
+        "too high"
+    elsif guess < number
+        "too low"
+    elsif guess == number
+        "you got it boi!"
+    end
+end
+
+number = rand(100)
+
+
+
 get '/' do
-    "The secret value is #{rand(0..100)}"
+    guess = params["guess"]
+    message = check_guess(number,params["guess"].to_i)
+    erb :index, :locals => {:number => number, :message => message, :guess => params["guess"]}
 end
